72835cf5b24f7dd13243ae8f7f3fa74f620bdb5f
sarkaaar <waliabdul144@gmail.com>
2021-08-26 09:12:17 +0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8f5cdd305b12bd6320d15cee8c399055'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a2751bf9-8e6d-4f99-ab61-adc5b85b4257'
+};
